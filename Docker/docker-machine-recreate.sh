0763df2efc09b0e137c4747923f0bbbe54d3059c
MartinFIT <marta.josefik@seznam.cz>
2018-01-07 10:35:24 +0100

    README ke spusteni Docker prostredi

--- Docker/docker-machine-recreate.sh
+++ Docker/docker-machine-recreate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker-machine rm default
-docker-machine create --driver virtualbox default
+docker-machine create --virtualbox-memory "2048" --driver virtualbox default
+eval "$(docker-machine env default)"
--- Docker/stop-docker-enviroment.sh
+++ Docker/stop-docker-enviroment.sh
@@ -3,7 +3,7 @@
 docker container stop cassandra
 docker container stop kafka
 docker container stop zookeeper
-docker container stop hadoop
+#docker container stop hadoop
 
 #cd Environment
 #docker-compose down
