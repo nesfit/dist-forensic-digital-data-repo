e27d8e4aa56857dee009c0cbb4342e668343aa72
MartinFIT <marta.josefik@seznam.cz>
2018-02-12 08:40:52 +0100

    Docker maven

--- Docker/docker-machine-recreate.sh
+++ Docker/docker-machine-recreate.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 docker-machine rm default
-docker-machine create --virtualbox-memory "2048" --virtualbox-share-folder "C:\\Docker:c/Docker" --driver virtualbox default
+docker-machine create --virtualbox-memory "4096" --virtualbox-share-folder "C:\\Users:c/Users" --driver virtualbox default
 eval "$(docker-machine env default)"
 
-docker-machine ssh default "mkdir /home/docker/Docker"
-docker-machine ssh default "sudo mount -t vboxsf -o uid=1000,gid=50 c/Docker /home/docker/Docker"
+docker-machine ssh default "mkdir /home/docker/Users"
+docker-machine ssh default "sudo mount -t vboxsf -o uid=1000,gid=50 c/Users /home/docker/Users"
+
+#-------------------------------------------
+
+#docker-machine rm default
+#docker-machine create --virtualbox-memory "4096" --virtualbox-share-folder "C:\\Docker:c/Docker" --driver virtualbox default
+#eval "$(docker-machine env default)"
+
+#docker-machine ssh default "mkdir /home/docker/Docker"
+#docker-machine ssh default "sudo mount -t vboxsf -o uid=1000,gid=50 c/Docker /home/docker/Docker"
--- Docker/install-docker-enviroment.sh
+++ Docker/install-docker-enviroment.sh
@@ -4,4 +4,8 @@ cd Environment
 
 docker-compose pull
 
+cd maven-jdk-9-slim-custom
+docker build -t martinfit/maven:3.5.2-jdk-9-slim .
+cd ..
+
 cd ..
