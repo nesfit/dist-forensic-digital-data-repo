063677ad774f56512cfd8f3d61cf50ca4aa84560
MartinFIT <xjosef00@stud.fit.vutbr.cz>
2018-02-26 15:35:15 +0100

    PacketRepository initialization of semaphore, removed old class for metadata

--- DIP_DistributedRepository/Persistence/src/main/java/cz/vutbr/fit/cassandra/repository/PacketRepositoryImpl.java
+++ DIP_DistributedRepository/Persistence/src/main/java/cz/vutbr/fit/cassandra/repository/PacketRepositoryImpl.java
@@ -1,12 +1,14 @@
 package cz.vutbr.fit.cassandra.repository;
 
 import com.datastax.driver.core.*;
+import com.datastax.driver.core.policies.LoadBalancingPolicy;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
 import cz.vutbr.fit.cassandra.entity.CassandraPacket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.AsyncCassandraTemplate;
+import org.springframework.util.Assert;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import javax.annotation.PostConstruct;
@@ -25,19 +27,35 @@ public class PacketRepositoryImpl implements InsertAsync {
     private static final String query = "INSERT INTO packet (id, packet) VALUES(?, ?);";
     private PreparedStatement statement;
 
+    private int maxRequestsPerConnection = 0;
     private Semaphore semaphore;
 
     @PostConstruct
     private void init() {
+        postConstructValidation();
+        postConstructInitialization();
+    }
+
+    private void postConstructValidation() {
+        Assert.notNull(cluster, "Cluster must be initialized");
+        Assert.notNull(session, "Session must be initialized");
+    }
+
+    private void postConstructInitialization() {
         asyncCassandraTemplate = new AsyncCassandraTemplate(session);
         statement = session.prepare(query);
 
+        LoadBalancingPolicy loadBalancingPolicy = cluster.getConfiguration().getPolicies().getLoadBalancingPolicy();
         PoolingOptions poolingOptions = cluster.getConfiguration().getPoolingOptions();
-        int maxRequestsPerConnectionLocal = poolingOptions.getMaxRequestsPerConnection(HostDistance.LOCAL);
-        int maxRequestsPerConnectionRemote = poolingOptions.getMaxRequestsPerConnection(HostDistance.REMOTE);
 
-        // TODO: Find appropriate parameter and remove hardcoded value!
-        semaphore = new Semaphore(maxRequestsPerConnectionLocal);
+        session.getState().getConnectedHosts().forEach(host -> {
+            HostDistance distance = loadBalancingPolicy.distance(host);
+            // TODO: Take maxRequestsPerConnection for the current host
+            maxRequestsPerConnection += poolingOptions.getMaxRequestsPerConnection(distance);
+        });
+
+        // TODO: Initialize semaphore to < maxConnectionsPerHost * maxRequestsPerConnection >
+        semaphore = new Semaphore(maxRequestsPerConnection);
     }
 
     @Override
@@ -48,40 +66,52 @@ public class PacketRepositoryImpl implements InsertAsync {
     }
 
     private void insertAsync(UUID id, ByteBuffer packet) {
-        try {
-            semaphore.acquire();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        lockSemaphore();
 
         // This approach with cached prepared statement is much faster
         BoundStatement boundStatement = statement.bind(id, packet);
         ResultSetFuture resultSetFuture = session.executeAsync(boundStatement);
+
         Futures.addCallback(resultSetFuture, new FutureCallback<ResultSet>() {
             @Override
             public void onSuccess(ResultSet rows) {
-                semaphore.release();
+                unlockSemaphore();
             }
 
             @Override
             public void onFailure(Throwable throwable) {
                 throwable.printStackTrace();
-                semaphore.release();
+                unlockSemaphore();
             }
         }, MoreExecutors.newDirectExecutorService());
     }
 
+    private void lockSemaphore() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void unlockSemaphore() {
+        semaphore.release();
+    }
+
     private void insertAsyncSpringData(CassandraPacket cassandraPacket) {
+        lockSemaphore();
+
         ListenableFuture<CassandraPacket> listenableFuture = asyncCassandraTemplate.insert(cassandraPacket);
-        listenableFuture.addCallback(PacketRepositoryImpl::onSuccess, PacketRepositoryImpl::onFailure);
+        listenableFuture.addCallback(this::onSuccess, this::onFailure);
     }
 
-    private static void onSuccess(CassandraPacket cassandraPacket) {
-
+    private void onSuccess(CassandraPacket cassandraPacket) {
+        unlockSemaphore();
     }
 
-    private static void onFailure(Throwable throwable) {
+    private void onFailure(Throwable throwable) {
         throwable.printStackTrace();
+        unlockSemaphore();
     }
 
 }
--- DIP_DistributedRepository/Persistence/src/main/java/cz/vutbr/fit/mongodb/repository/InsertAsync.java
+++
@@ -1,9 +0,0 @@
-package cz.vutbr.fit.mongodb.repository;
-
-import cz.vutbr.fit.mongodb.entity.PacketMetadata;
-
-public interface InsertAsync {
-
-    public void insertAsync(PacketMetadata packetMetadata);
-
-}
