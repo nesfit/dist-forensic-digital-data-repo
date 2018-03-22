06fc4ddf0874000ad233dfbd67d967877d36cefc
MartinFIT <xjosef00@stud.fit.vutbr.cz>
2018-03-22 16:58:10 +0100

    Added dynamic properties to run scripts

--- DIP_DistributedRepository/DistributedRepository/run.sh
+++ DIP_DistributedRepository/DistributedRepository/run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+VM_IP=192.168.99.100
+SPRING_DATA_CASSANDRA_CONTACT_POINTS=$VM_IP
+
 HADOOP_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' hadoop`
 HDFS_PORT=9000
 SPRING_HADOOP_FS_URI=hdfs://$HADOOP_IP:$HDFS_PORT
+
+KAFKA_PORT=9092
+SPRING_KAFKA_BOOTSTRAP_SERVERS=$VM_IP:$KAFKA_PORT
+SPRING_KAFKA_PRODUCER_BOOTSTRAP_SERVERS=$VM_IP:$KAFKA_PORT
+
+SPRING_DATA_MONGODB_HOST=$VM_IP
 
 docker run \
 	-it \
@@ -13,6 +22,10 @@ docker run \
 	-w "/usr/src/app/" \
 	martinfit/maven:3.5.2-jdk-9-slim \
 		java \
+			-Dspring.data.cassandra.contact-points=$SPRING_DATA_CASSANDRA_CONTACT_POINTS \
 			-Dspring.hadoop.fs-uri=$SPRING_HADOOP_FS_URI \
+			-Dspring.kafka.bootstrap-servers=$SPRING_KAFKA_BOOTSTRAP_SERVERS \
+			-Dspring.kafka.producer.bootstrap-servers=$SPRING_KAFKA_PRODUCER_BOOTSTRAP_SERVERS \
+			-Dspring.data.mongodb.host=$SPRING_DATA_MONGODB_HOST \
 			-cp distributed-repository-1.0-SNAPSHOT.jar \
 			-jar target/distributed-repository-1.0.jar
--- DIP_DistributedRepository/ProducerApp/run.sh
+++ DIP_DistributedRepository/ProducerApp/run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+VM_IP=192.168.99.100
+
 HADOOP_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' hadoop`
 HDFS_PORT=9000
 SPRING_HADOOP_FS_URI=hdfs://$HADOOP_IP:$HDFS_PORT
+
+KAFKA_PORT=9092
+SPRING_KAFKA_BOOTSTRAP_SERVERS=$VM_IP:$KAFKA_PORT
+SPRING_KAFKA_PRODUCER_BOOTSTRAP_SERVERS=$VM_IP:$KAFKA_PORT
 
 docker run \
 	-it \
@@ -14,5 +20,7 @@ docker run \
 	martinfit/maven:3.5.2-jdk-9-slim \
 		java \
 			-Dspring.hadoop.fs-uri=$SPRING_HADOOP_FS_URI \
+			-Dspring.kafka.bootstrap-servers=$SPRING_KAFKA_BOOTSTRAP_SERVERS \
+			-Dspring.kafka.producer.bootstrap-servers=$SPRING_KAFKA_PRODUCER_BOOTSTRAP_SERVERS \
 			-cp producer-app-1.0-SNAPSHOT.jar \
 			-jar target/producer-app-1.0.jar target/classes/PCAP
