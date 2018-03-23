92940ccdd51e7257088a20da6f2e3b3c337406ed
MartinFIT <marta.josefik@seznam.cz>
2018-03-23 10:02:13 +0100

    Readable scripts

--- DIP_DistributedRepository/Communication/install.sh
+++ DIP_DistributedRepository/Communication/install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-docker run -it --rm --name communication -v "$HOME/.m2":/root/.m2 -v "$PWD":/usr/src/app -w "/usr/src/app/" martinfit/maven:3.5.2-jdk-9-slim mvn clean install
+docker run \
+	-it \
+	--rm \
+	--name communication \
+	-v "$PWD":/usr/src/app \
+	-v "$HOME/.m2":/root/.m2 \
+	-w "/usr/src/app/" \
+	martinfit/maven:3.5.2-jdk-9-slim \
+		mvn clean install
